9f25053ec00bfc48005808bebe82103299f454fb
unknown <Max>
2024-05-23 17:19:00 +0300

    commit

--- DjangoTest/django_test_app/urls.py
+++ DjangoTest/django_test_app/urls.py
@@ -2,18 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path("fourth/", views.form_page, name='fourth'),
-    path("user/", views.user_page, name="user"),
     path('home/', views.HomePage.as_view(), name='home'),
-    path("showpost/<int:id>/", views.ShowPost.as_view(), name="getpost"),
     path("post/<int:pk>/", views.PostPage.as_view(), name="post"),
-    path("postpage/", views.post_page, name="postpage"),
     path("registration/", views.RegistrationPage.as_view(), name="registration"),
     path("logout/", views.logout_view, name="logout"),
     path("login/", views.LoginPage.as_view(), name="login"),
     path("postcreate/", views.PostCreate.as_view(), name="createpost"),
-    path("create_comment<int:id>/", views.CreateComment.as_view(), name="createcomment"),
-    path("profile/<int:pk>/", views.ProfilePage.as_view()),
-    path("js/", views.Js.as_view(), name="js"),
-    path("ajax/", views.ajaxpage.as_view())]
+    path("profile/<int:pk>/", views.ProfilePage.as_view())]
--- DjangoTest/django_test_app/views.py
+++ DjangoTest/django_test_app/views.py
@@ -14,34 +14,10 @@ from django.template.loader import render_to_string
 from django.views.generic.detail import DetailView
 
 
-def home(request):
-    a = Post.objects.all()
-    return render(request, 'home.html', {'posts': a, 'title': 'Home', 'user': request.user})
-
-
 class HomePage(ListView):
     template_name = 'home.html'
     model = Post
     context_object_name = 'post'
-
-
-class Js(TemplateView):
-    template_name = "js.html"
-
-
-
-def form_page(request):
-    if request.method == 'POST':
-        form = Registration(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password1']
-            user = User.objects.create_user(username=name, email=email, password=password)
-            user.save()
-            return redirect('/')
-    else:
-        return render(request, 'registration.html', {'form': Registration()})
 
 
 class RegistrationPage(CreateView):
@@ -80,27 +56,6 @@ class PostPage(DetailView):
         return JsonResponse(response, safe=False)
 
 
-class CreateComment(TemplateView):
-    template_name = "post.html"
-
-    def post(self, request, **kwargs):
-        data = request.POST
-        if data.get['post']:
-            pass
-        elif data['post']:
-            #update_post
-            pass
-        return redirect()
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        form.instance.post = Post.objects.get(id=self.kwargs["id"])
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return f'/post/{self.kwargs["id"]}'
-
-
 class PostCreate(CreateView):
     form_class = PostForm
     template_name = 'postcreate.html'
@@ -109,44 +64,6 @@ class PostCreate(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-
-
-class ShowPost(TemplateView):
-    template_name = "post.html"
-    # model = Post
-    # form_class = PostForm
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        post = Post.objects.get(id=self.kwargs["id"])
-        context['post'] = post
-        context['comment'] = Comment.objects.filter(post=post)
-        return context
-
-
-def post_page(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            text = form.cleaned_data["text"]
-            post = Post(text=text, user=request.user)
-            post.save()
-            return redirect("home")
-            print(text)
-    else:
-        return render(request, "post.html", {"form": PostForm()})
-
-
-def user_page(request):
-    if request.method == "POST":
-        form = UserForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            surname = form.cleaned_data['surname']
-            age = form.cleaned_data['age']
-            print(name, surname, age)
-    else:
-        return render(request, 'form.html', {'form': UserForm()})
 
 
 @login_required
@@ -159,11 +76,6 @@ class LoginPage(LoginView):
     template_name = 'login.html'
     form_class = Login
     redirect_authenticated_user = True
-
-
-@login_required
-def profile(request):
-    return render(request, 'profile.html', {'user': request.user, 'profile': Profile.objects.get(user=request.user)})
 
 
 class ProfilePage(UpdateView):
@@ -184,12 +96,7 @@ class ProfilePage(UpdateView):
         return f'/profile/{self.kwargs["pk"]}'
 
 
-class ajaxpage(TemplateView):
-    template_name = 'page.html'
 
-    def post(self, request):
-        print(request.POST)
-        return JsonResponse({'status': 'OK'})
 
 
 
